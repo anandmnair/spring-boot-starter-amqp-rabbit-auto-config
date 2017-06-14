f990fe75878e66570f08c27a9132036910918102
anandmnair <anand.manissery@gmail.com>
2017-06-14 22:54:13 +0530

    Removed unwanted main class

--- src/test/java/org/springframework/amqp/SpringBootStarterAmqpRabbitAutoConfigApplicationTests.java
+++ src/test/java/org/springframework/amqp/SpringBootStarterAmqpRabbitAutoConfigApplicationTests.java
@@ -10,7 +10,7 @@ import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes={SpringBootStarterAmqpRabbitAutoConfigApplication.class})
+@SpringBootTest
 public class SpringBootStarterAmqpRabbitAutoConfigApplicationTests {
 
 	@Autowired
