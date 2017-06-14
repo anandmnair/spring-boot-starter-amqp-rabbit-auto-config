dd904b17db584d6d51ef74a693a5ec9a938ab739
anandmnair <anand.manissery@gmail.com>
2017-06-14 23:02:51 +0530

    Adding spring boot annotation in context test

--- src/test/java/org/springframework/amqp/SpringBootStarterAmqpRabbitAutoConfigApplicationTests.java
+++ src/test/java/org/springframework/amqp/SpringBootStarterAmqpRabbitAutoConfigApplicationTests.java
@@ -5,12 +5,14 @@ import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@SpringBootApplication
 public class SpringBootStarterAmqpRabbitAutoConfigApplicationTests {
 
 	@Autowired
@@ -29,5 +31,4 @@ public class SpringBootStarterAmqpRabbitAutoConfigApplicationTests {
 		assertTrue(applicationContext.containsBean("queue-two.dlq"));
 	}
 	
-
 }
